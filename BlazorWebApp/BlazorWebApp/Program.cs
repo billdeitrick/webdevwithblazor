85e054241c6213ab1406e97c2970b5d3592ea22b
Bill Deitrick <4174527+billdeitrick@users.noreply.github.com>
2024-11-20 23:37:41 -0500

    Chapter 8 checkpoint

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -1,9 +1,14 @@
+using BlazorWebApp;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using BlazorWebApp.Client.Pages;
 using BlazorWebApp.Components;
 using BlazorWebApp.Endpoints;
 using Data;
 using Data.Models.Interfaces;
 using System.IO;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +30,14 @@ builder.Services.AddOptions<BlogApiJsonDirectAccessSetting>().Configure(
 );
 builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
 
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingServerAuthenticationStateProvider>();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Authority"] ?? "";
+    options.ClientId = builder.Configuration["Auth0:ClientId"] ?? "";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,6 +56,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
@@ -54,5 +69,24 @@ app.MapBlogPostAPI();
 app.MapCategoryApi();
 app.MapTagApi();
 app.MapCommentApi();
+
+app.MapGet("account/login", async (string returnUrl, HttpContext context) =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+        .WithRedirectUri(returnUrl)
+        .Build();
+    
+    await context.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("authentication/logout", async (HttpContext context) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+        .WithRedirectUri("/")
+        .Build();
+    
+    await context.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.Run();
